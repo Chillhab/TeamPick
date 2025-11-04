3447930887b01816042eb51ba0b71f75e9295ef2
Chillhab <c.habbachi@gmail.com>
2025-11-04 13:32:14 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,12 @@
+HT 16 – Match Organizer (PWA minimal)
+=================================
+Diese ZIP ist entpackbar und geeignet für GitHub Pages (Minimaltest).
+
+Inhalt:
+- index.html
+- manifest.json
+- service-worker.js
+- icons/icon-192.png
+- icons/icon-512.png
+
+Stand: 2025-10-24
---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("ht16-cache-v2").then((cache) => cache.addAll([
+      "./",
+      "./index.html",
+      "./manifest.json",
+      "./icons/icon-192.png",
+      "./icons/icon-512.png"
+    ]))
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
